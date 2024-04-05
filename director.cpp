8b56e26fc0efc0320eb5588cff97fb9c231ec9a9
gasparaveti7 <147263551+gasparaveti7@users.noreply.github.com>
2024-04-05 23:18:06 +0300

    Add files via upload

---
+++ director.cpp
@@ -0,0 +1,14 @@
+#include "director.h"
+
+Director::Director() {
+	Worker();
+}
+
+Director::Director(int _age, int _phone_number, std::string _name, std::string _surname, std::string _email, int sal) {
+	age = _age;
+	phone_number = _phone_number;
+	name = _name;
+	surname = _surname;
+	email = _email;
+	salary = sal;
+}
